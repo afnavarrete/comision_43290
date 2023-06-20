0f3f389a39f2dac2dcd0ceb73365f6d3d320ce2c
antonela foresti <forestiantonela@gmail.com>
2023-06-19 22:10:21 -0300

    clase_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+//Importaciones
+import Footer from "./Footer";
 import Navbar from "./Navbar";
-
+import Home from "./Home";
+//--------------------------------------------
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Este es el App</h1>
+      <Footer />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Este es el Home</h1>;
+};
+
+export default Home;
